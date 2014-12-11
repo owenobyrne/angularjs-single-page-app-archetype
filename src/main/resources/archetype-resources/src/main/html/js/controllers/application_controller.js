92bf71192feb06911566b64784484d1ff989683a
Owen O Byrne <owen.obyrne@iol.ie>
2014-12-11 23:33:36 +0000

    Updated to use most recent versions of JS libraries from CDN.

--- src/main/resources/archetype-resources/src/main/html/js/controllers/application_controller.js
+++ src/main/resources/archetype-resources/src/main/html/js/controllers/application_controller.js
@@ -1,30 +1,28 @@
 function ApplicationCtrl($scope, $rootScope, User, Session, $state, $timeout) {
 
+	var socket = new SockJS('/aibaccountmonitor/websockets/portfolio');
+    var stompClient = Stomp.over(socket);
+    stompClient.connect({}, function(frame) {
+        console.log('Connected ');
+        console.log(frame);
+        
+        stompClient.subscribe("/greeting", function(message) {
+        	console.log(message.body);
+        	//$rootScope.$broadcast("notification", data);
 
+        });
+        
+        
+    }, function(error) {
+        console.log("STOMP protocol error " + error);
+    });
+    
+	$scope.$on("notification", function(event, notification) {
+		if (notification.type == "some-notification") {
+			$state.go("application.page1");
 
-	var sockjs = new SockJS("http://node.foursevensix.com/sockjs", null, { debug: true, devel: true });
-
-
-	    sockjs.onopen    = function()  {console.log('[*] open', sockjs.protocol);};
-
-	    sockjs.onmessage = function(e) {
-	    	$scope.$apply(function() {
-	    		var data = $.parseJSON(e.data);
-	 			console.log(data);
-
-	 			//$scope.notifications.unshift(data);
-	 			$rootScope.$broadcast("notification", data);
-
-	    	});
-	    };
-
-		$scope.$on("notification", function(event, notification) {
-			if (notification.type == "some-notification") {
-				$state.go("application.page1");
-
-			}
-		});
+		}
+	});
 }
-
 //ApplicationCtrl.$inject = ['$scope', 'User', 'Session'];
 
